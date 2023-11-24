a16218c42e905d18a7e808dd6b50f22ac723e294
aliciamurma <102992210+aliciamurma@users.noreply.github.com>
2023-11-24 09:49:16 +0000

    imaginary

--- imaginary.py
+++ imaginary.py
@@ -1,12 +1,39 @@
 from library import *
+import re
+
+
+def ft_get_imaginary(var):
+    # Patrón de expresión regular para buscar coeficientes imaginarios
+    coincidence = re.search(r'([-+]?\s*\d*)i', var)
+    if coincidence:
+        # Obtener el coeficiente imaginario
+        nbr_i = coincidence.group(1)
+        
+        # Si el coeficiente es vacío o solo tiene un signo, asignarle 1
+        if not nbr_i or nbr_i == '+' or nbr_i == '-':
+            nbr_i = '1'
+        
+        # Si el coeficiente es '-' sin dígito, asignarle -1
+        elif nbr_i == '-':
+            nbr_i = '-1'
+        
+        # Eliminar espacios en blanco y convertir el coeficiente a un número
+        nbr_i = int(nbr_i.replace(' ', ''))
+        
+        # Verificar el signo
+        signo = -1 if var[0] == '-' else 1
+        return nbr_i * signo
+    return 0
+
+def ft_print_imaginary(var):
+    print(ft_get_imaginary(var), "i")
 
 def ft_save_imaginary(var):
     print("Save imaginary")
     var2 = var.split('=')
     new_var = MyVar(var2[0], var2[1])
     variables[var2[0]] = new_var  # Add the new variable to the 'variables' dictionary
-    print("Save rational")
-    print(ft_find_variable(variables, var2[0]))
+    ft_print_imaginary(var2[1])
 
 def ft_is_imaginary(var):
     print("Inside is imaginary")
