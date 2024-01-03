d88bc2ea5fd8128e3e55459029fc0ebf8bb08b44
aliciamurma <aliciamurciamaya@gmail.com>
2024-01-03 19:59:30 +0100

    operations ok

--- operations.py
+++ operations.py
@@ -3,7 +3,7 @@ from aux import *
 from function import *
 import re
 
-def ft_operate_numeric(expression):
+def ft_operate_numeric2(expression):
     operandos = []
     operadores = []
     i = 0
@@ -24,14 +24,16 @@ def ft_operate_numeric(expression):
                 i += 1
             operandos.append(float(expression[inicio:i]))
 
-        elif expression[i] in "+-*/":
+        elif expression[i] in "+-*/%":
             # Manejar operadores
-            while operadores and operadores[-1] in "*/" and (expression[i] in "+-"):
+            while operadores and operadores[-1] in "*/%" and (expression[i] in "+-"):
                 operador = operadores.pop()
                 operando2 = operandos.pop()
                 operando1 = operandos.pop()
                 if operador == '*':
                     operandos.append(operando1 * operando2)
+                if operador == '%':
+                    operandos.append(operando1 % operando2)
                 elif operador == '/':
                     if operando2 != 0:
                         operandos.append(operando1 / operando2)
@@ -61,6 +63,8 @@ def ft_operate_numeric(expression):
                         operandos.append(operando1 / operando2)
                     else:
                         return "Error: No se puede dividir por cero."
+                elif operador == '%':
+                    operandos.append(operando1 % operando2)
             operadores.pop()  # Sacar el paréntesis izquierdo
             i += 1
 
@@ -74,23 +78,104 @@ def ft_operate_numeric(expression):
         if operador == '+':
             if negative == 1 and nbr_neg.strip() and operando1 == float(nbr_neg.strip()):
                 negative = 0
+                print("-", operando1, "+", operando2)
                 operandos.append(- operando1 + operando2)
             else:
                 operandos.append(operando1 + operando2)
+                print(operando1, "+", operando2)
         elif operador == '-':
             if negative == 1 and nbr_neg.strip() and operando1 == float(nbr_neg.strip()):
                 negative = 0
                 operandos.append(- operando1 - operando2)
+                print("-", operando1, "-", operando2)
             else:
                 operandos.append(operando1 - operando2)
+                print(operando1, "-", operando2)
         elif operador == '*':
             operandos.append(operando1 * operando2)
+            print(operando1, "*", operando2)
         elif operador == '/':
             if operando2 != 0:
                 operandos.append(operando1 / operando2)
+                print(operando1, "/", operando2)
             else:
                 return "Error: No se puede dividir por cero."
     return operandos[0]
+
+def ft_operate_numeric(expression):
+    operandos = []
+    operadores = []
+    i = 0
+    negative = 0
+    nbr_neg = 0
+
+    while i < len(expression):
+        if i == 0 and expression[0] == '+':
+            i += 1
+        if i == 0 and expression[0] == '-':
+            negative = 1
+            nbr_neg = expression[2]
+            i += 1
+        if expression[i].isdigit() or (i < len(expression) - 1 and expression[i] == '-' and expression[i + 1].isdigit()):
+            # Extraer números (manejar números negativos)
+            inicio = i
+            while i < len(expression) and (expression[i].isdigit() or expression[i] == '.'):
+                i += 1
+            operandos.append(float(expression[inicio:i]))
+
+        elif expression[i] in "+-*/%":
+            # Manejar operadores
+            while operadores and operadores[-1] in "*/%" and (expression[i] in "+-"):
+                operador = operadores.pop()
+                operando2 = operandos.pop()
+                operando1 = operandos.pop()
+                if operador == '*':
+                    operandos.append(operando1 * operando2)
+                if operador == '%':
+                    operandos.append(operando1 % operando2)
+                elif operador == '/':
+                    if operando2 != 0:
+                        operandos.append(operando1 / operando2)
+                    else:
+                        return "Error: No se puede dividir por cero."
+
+            operadores.append(expression[i])
+            i += 1
+
+        elif expression[i] == '(':
+            operadores.append(expression[i])
+            i += 1
+
+        elif expression[i] == ')':
+            while operadores and operadores[-1] != '(':
+                operador = operadores.pop()
+                operando2 = operandos.pop()
+                operando1 = operandos.pop()
+                if operador == '+':
+                    operandos.append(operando1 + operando2)
+                elif operador == '-':
+                    operandos.append(operando1 - operando2)
+                elif operador == '*':
+                    operandos.append(operando1 * operando2)
+                elif operador == '/':
+                    if operando2 != 0:
+                        operandos.append(operando1 / operando2)
+                    else:
+                        return "Error: No se puede dividir por cero."
+                elif operador == '%':
+                    operandos.append(operando1 % operando2)
+            operadores.pop()  # Sacar el paréntesis izquierdo
+            i += 1
+
+        else:
+            i += 1
+    while operadores:
+        operando2 = operandos.pop()
+        operando1 = operandos.pop()
+        operador = operadores.pop()
+        print("operando 1: ", operando1)
+        print("operando 2: ", operando2)
+        print("operador: ", operador)
 
 def ft_isletter(var):
     return any(caracter.isalpha() for caracter in var)
@@ -113,7 +198,7 @@ def ft_operate(var):
     separated = ft_separate(value)
     replaced = ft_replace_variables(separated)
     if ft_isletter(replaced) is False:
-        print(ft_operate_numeric(replaced))
+        print(eval(replaced))
     elif ft_one_letter(replace) is True:
         print("No please, no.")
     else:
